9bc99f29b1db8c5e47987da4f0e0a3ec8b01955e
peppermint100 <krpeppermint100@gmail.com>
2024-03-30 10:26:42 +0900

    [Build] Storyboard 삭제

---
+++ AppStoreClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreClone
+//
+//  Created by peppermint100 on 3/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
